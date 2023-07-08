fcf3bca78d8282391e9dbfcdd4330e9bbe9bd4f4
weijian <weijian9091@gmail.com>
2023-07-09 00:33:36 +0800

    Finish the basic logic of the assignment

--- scripts.js
+++ scripts.js
@@ -3,101 +3,78 @@ const player = (name, mark) => {
   let isWinner = false;
   return { name, mark, isWinner };
 };
+const displayController = () => {
+  let turn = document.querySelector(".player-turn");
+  let grids = document.getElementsByClassName("grid-item");
+  let button = document.querySelector(".start-game");
+  return {
+    turn,
+    grids,
+    button,
+  };
+};
 
 const gameBoard = (() => {
-  let board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
+  let board = ["", "", "", "", "", "", "", "", ""];
   let player1 = player("weijian", "X");
   let player2 = player("xinyu", "O");
-  let winningPattern = "";
-  let winnerFound = false;
+  let currentPlayer = player1;
+  let { turn, grids, button } = displayController();
+
+  const resetGame = () => {
+    board = ["", "", "", "", "", "", "", "", ""];
+    turn.textContent = "";
+    for (let grid of grids) {
+      grid.textContent = "";
+      grid.removeEventListener("click", clickHandler);
+    }
+  };
 
   const winningConditions = [
     // Horizontal
-    [
-      [0, 0],
-      [0, 1],
-      [0, 2],
-    ],
-    [
-      [1, 0],
-      [1, 1],
-      [1, 2],
-    ],
-    [
-      [2, 0],
-      [2, 1],
-      [2, 2],
-    ],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
     // Vertical
-    [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-    ],
-    [
-      [0, 1],
-      [1, 1],
-      [2, 1],
-    ],
-    [
-      [0, 2],
-      [1, 2],
-      [2, 2],
-    ],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
     // Diagonal
-    [
-      [0, 0],
-      [1, 1],
-      [2, 2],
-    ],
-    [
-      [0, 2],
-      [1, 1],
-      [2, 0],
-    ],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
 
   const checkWin = () => {
     for (let condition of winningConditions) {
       const [a, b, c] = condition;
-      const [x1, y1] = a;
-      const [x2, y2] = b;
-      const [x3, y3] = c;
-      if (
-        board[x1][y1] !== "" &&
-        board[x1][y1] === board[x2][y2] &&
-        board[x1][y1] === board[x3][y3]
-      ) {
-        winnerFound = true;
-        winningPattern = board[x1][y1];
-        return true; // Game is won
+      if (!isEmpty(a) && board[a] === board[b] && board[a] === board[c]) {
+        return true;
       }
     }
-    return false; // Game not won
+    return false;
   };
 
-  const isEmpty = (row, column) => {
-    let symbols = ["X", "O"];
-    return !symbols.includes(board[row][column]);
+  const isEmpty = (gridNumber) => {
+    return board[gridNumber] === "";
+  };
+  const hasEmptyGrids = () => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i] === "") {
+        return true;
+      }
+    }
+    return false;
   };
 
-  const addMark = (row, column, mark) => {
-    board[row][column] = mark;
+  const announceDraw = () => {
+    alert("Game is a draw");
   };
+
   const isDraw = () => {
-    for (let row of board) {
-      for (let cell of row) {
-        if (cell === "") {
-          return false; // Empty cell found, game is not a draw
-        }
-      }
+    if (!hasEmptyGrids() && !checkWin()) {
+      return true;
     }
-
-    return !checkWin(); // All cells are filled, check if it's not a win
+    return false;
   };
 
   const gameOver = () => {
@@ -109,46 +86,38 @@ const gameBoard = (() => {
     }
     return false;
   };
+  function clickHandler(event) {
+    event.target.textContent = currentPlayer.mark;
+    let index = parseInt(event.target.id.split("-")[1]);
+    board[index] = currentPlayer.mark;
+    if (checkWin()) {
+      congratulateWinner();
+      resetGame();
+    } else if (isDraw()) {
+      announceDraw();
+      resetGame();
+    } else {
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+      turn.textContent = `${currentPlayer.name}'s turn.`;
+    }
+  }
   const runGame = () => {
-    while (!gameOver()) {
-      let playerOneRow = prompt("Player 1 row: ");
-      let playerOneCol = prompt("Player 1 column: ");
-      while (!isEmpty(playerOneRow, playerOneCol)) {
-        playerOneRow = prompt("Player 1 row: ");
-        playerOneCol = prompt("Player 1 column: ");
-      }
-      addMark(playerOneRow, playerOneCol, player1.mark);
-      if (gameOver()) {
-        congratulateWinner();
-        return;
-      }
-
-      let playerTwoRow = prompt("Player 2 row: ");
-      let playerTwoCol = prompt("Player 2 column: ");
-      while (!isEmpty(playerTwoRow, playerTwoCol)) {
-        playerTwoRow = prompt("Player 2 row: ");
-        playerTwoCol = prompt("Player 2 column: ");
-      }
-      addMark(playerTwoRow, playerTwoCol, player2.mark);
-
-      console.log(board);
-      if (gameOver()) {
-        congratulateWinner();
-        return;
+    turn.textContent = `${player1.name}'s turn.`;
+    for (let i = 0; i < grids.length; i++) {
+      if (isEmpty(i)) {
+        grids[i].addEventListener("click", clickHandler);
       }
     }
-    congratulateWinner();
   };
   const congratulateWinner = () => {
-    if (gameOver() && winnerFound) {
-      if (winningPattern === "X") {
-        console.log(`Congratulations, ${player1.name} has won the game`);
-      } else {
-        console.log(`Congratulations, ${player2.name} has won the game`);
-      }
-    } else {
-      console.log("The game ended in a draw.");
-    }
+    alert(`Congratulations, ${currentPlayer.name}! You have won the game`);
   };
-  return { runGame, board, gameOver, checkWin, isDraw };
+  return {
+    runGame,
+    board,
+    gameOver,
+    checkWin,
+  };
 })();
+const startButton = document.querySelector(".start-game");
+startButton.addEventListener("click", gameBoard.runGame);
