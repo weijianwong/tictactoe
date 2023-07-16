46c92447d0ffeb85e3af0f0fa2cc8f3b9ad666e6
weijian <weijian9091@gmail.com>
2023-07-16 14:24:12 +0800

    Added ai for the game using minimax

--- scripts.js
+++ scripts.js
@@ -35,13 +35,15 @@ const gameBoard = (() => {
     hasEmptyGrids,
     isEmpty,
     resetBoard,
+    board,
   };
 })();
 
 const gameController = (() => {
   const player1 = player("Player 1", "X");
   const player2 = player("Player 2", "O");
-  const players = [player1, player2];
+  const computer = player("Computer", "O");
+  let players = [player1, player2];
   let winner = null;
   let winnerFound = false;
   let gameIsDraw = false;
@@ -62,8 +64,6 @@ const gameController = (() => {
   ];
   let activePlayer = players[0];
 
-  const printBoard = () => console.log(board.getBoard());
-
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
@@ -75,14 +75,16 @@ const gameController = (() => {
 
   const getGameIsDraw = () => gameIsDraw;
 
-  const isDraw = () => {
-    if (!board.hasEmptyGrids() && !winnerFound) {
-      return true;
-    }
-    return false;
+  const setPlayers = () => {
+    players = [player1, computer];
   };
 
-  const checkWin = () => {
+  const gameEnded = () => {
+    if (!board.hasEmptyGrids() && !winnerFound) {
+      gameIsDraw = true;
+      return true;
+    }
+
     for (let condition of winningConditions) {
       const [a, b, c] = condition;
       if (
@@ -90,6 +92,38 @@ const gameController = (() => {
         currentBoard[a] === currentBoard[b] &&
         currentBoard[a] === currentBoard[c]
       ) {
+        if (currentBoard[a] === "X") {
+          winner = players[0];
+        } else {
+          winner = players[1];
+        }
+        winnerFound = true;
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const gameDraw = () => {
+    if (!gameWon() && !board.hasEmptyGrids()) {
+      return true;
+    }
+    return false;
+  };
+  let simulatedWinner;
+  const gameWon = () => {
+    for (let condition of winningConditions) {
+      const [a, b, c] = condition;
+      if (
+        !board.isEmpty(a) &&
+        currentBoard[a] === currentBoard[b] &&
+        currentBoard[a] === currentBoard[c]
+      ) {
+        if (currentBoard[a] === "X") {
+          simulatedWinner = players[0];
+        } else {
+          simulatedWinner = players[1];
+        }
         return true;
       }
     }
@@ -100,39 +134,112 @@ const gameController = (() => {
     board.resetBoard();
     currentBoard = board.getBoard();
     winner = null;
+    players = [player1, player2];
     activePlayer = players[0];
     gameIsDraw = false;
     winnerFound = false;
+    simulatedWinner = null;
+  };
+
+  function getNextPlayer(currentPlayer) {
+    return currentPlayer === player1 ? computer : player1;
+  }
+
+  function getAvailableMoves(currentBoard) {
+    let availableMoves = [];
+    for (let i = 0; i < currentBoard.length; i++) {
+      if (currentBoard[i] === "") {
+        availableMoves.push(i);
+      }
+    }
+    return availableMoves;
+  }
+
+  function getScore() {
+    if (gameWon() && simulatedWinner === player1) {
+      return 10;
+    } else if (gameWon() && simulatedWinner === computer) {
+      return -10;
+    } else {
+      return 0;
+    }
+  }
+
+  function minimax(boardNow, playerNow) {
+    if (gameWon() || gameDraw()) {
+      let score = getScore();
+      return { score: score, index: null };
+    }
+    let movesAvail = getAvailableMoves(boardNow);
+    let bestScore = playerNow === computer ? 100 : -100;
+    let bestMove;
+    // for maximizing player
+    if (playerNow === player1) {
+      for (let move of movesAvail) {
+        currentBoard[move] = playerNow.mark; // Simulate the move
+        let newScore = minimax(currentBoard, getNextPlayer(player1)).score; // Recursive call
+        currentBoard[move] = "";
+        if (newScore > bestScore) {
+          bestScore = newScore;
+          bestMove = move;
+        }
+      }
+      return { score: bestScore, index: bestMove };
+    }
+    // for minimizing player
+    else {
+      for (let move of movesAvail) {
+        currentBoard[move] = playerNow.mark; // Simulate the move
+        let newScore = minimax(currentBoard, getNextPlayer(computer)).score; // Recursive call
+        currentBoard[move] = "";
+        if (newScore < bestScore) {
+          bestScore = newScore;
+          bestMove = move;
+        }
+      }
+      return { score: bestScore, index: bestMove };
+    }
+  }
+
+  const playOneRoundWithComputer = (gridNumber) => {
+    playOneRound(gridNumber);
+    if (gameEnded()) {
+      return;
+    }
+    let computerMove = minimax(currentBoard, computer).index;
+    playOneRound(computerMove);
   };
 
   const playOneRound = (gridNumber) => {
     board.placeMarker(gridNumber, activePlayer.mark);
-    if (checkWin()) {
-      winnerFound = true;
-      winner = activePlayer;
-    }
-    if (isDraw()) {
-      gameIsDraw = true;
+    if (gameEnded()) {
+      return;
     }
     switchPlayerTurn();
-    printBoard();
   };
   return {
+    getBoard: board.getBoard,
     resetGame,
     playOneRound,
     getActivePlayer,
     getWinner,
     getWinnerFound,
     getGameIsDraw,
+    setPlayers,
+    playOneRoundWithComputer,
   };
 })();
 
 const ScreenController = () => {
   let game = gameController;
+  let gameStarted = false;
+  let playingWithComputer = false;
   let turn = document.querySelector(".player-turn");
   let grids = document.getElementsByClassName("grid-item");
   let startButton = document.querySelector(".start-game");
   let mainContent = document.querySelector(".main-content");
+  let vsComputerButton = document.querySelector(".vs-computer");
+  let resetGameButton = document.querySelector(".reset-game");
 
   const announceResult = () => {
     mainContent.classList.add("disabled");
@@ -156,20 +263,25 @@ const ScreenController = () => {
   };
 
   const resetBoard = () => {
-    startButton.textContent = "Start game";
-    startButton.removeEventListener("click", resetBoard);
-    startButton.addEventListener("click", startGame);
+    updateButtons();
     game.resetGame();
     turn.textContent = "";
-    for (let grid of grids) {
-      grid.textContent = "";
-      grid.removeEventListener("click", clickHandler);
+    if (playingWithComputer) {
+      for (let grid of grids) {
+        grid.textContent = "";
+        grid.removeEventListener("click", clickForComputer);
+      }
+    } else {
+      for (let grid of grids) {
+        grid.textContent = "";
+        grid.removeEventListener("click", clickHandler);
+      }
     }
+    playingWithComputer = false;
   };
 
   function clickHandler(event) {
     if (event.target.textContent === "") {
-      event.target.textContent = game.getActivePlayer().mark;
       let index = parseInt(event.target.id.split("-")[1]);
       game.playOneRound(index);
       updateScreen();
@@ -180,19 +292,63 @@ const ScreenController = () => {
       }
     }
   }
+  function clickForComputer(event) {
+    if (event.target.textContent === "") {
+      let index = parseInt(event.target.id.split("-")[1]);
+      game.playOneRoundWithComputer(index);
+      updateScreen();
+      if (game.getGameIsDraw()) {
+        announceResult();
+      } else if (game.getWinnerFound()) {
+        announceResult();
+      }
+    }
+  }
+
   const startGame = () => {
-    startButton.addEventListener("click", updateScreen);
+    resetGameButton.style.display = "none";
+    startButton.addEventListener("click", () => {
+      gameStarted = true;
+      updateScreen();
+      updateButtons();
+    });
+    vsComputerButton.addEventListener("click", () => {
+      gameStarted = true;
+      playingWithComputer = true;
+      game.setPlayers();
+      updateScreen();
+      updateButtons();
+    });
+  };
+
+  const updateButtons = () => {
+    if (gameStarted) {
+      startButton.style.display = "none";
+      vsComputerButton.style.display = "none";
+      resetGameButton.style.display = "block";
+      resetGameButton.addEventListener("click", resetBoard);
+      gameStarted = false;
+    } else {
+      startButton.style.display = "block";
+      vsComputerButton.style.display = "block";
+      resetGameButton.style.display = "none";
+    }
   };
   const updateScreen = () => {
-    startButton.textContent = "Reset game";
-    startButton.removeEventListener("click", updateScreen);
-    startButton.addEventListener("click", resetBoard);
+    let board = game.getBoard();
+
     const activePlayer = game.getActivePlayer();
 
     turn.textContent = `${activePlayer.name}'s turn.`;
 
-    for (let i = 0; i < grids.length; i++) {
-      grids[i].addEventListener("click", clickHandler);
+    for (let i = 0; i < 9; i++) {
+      if (playingWithComputer) {
+        grids[i].addEventListener("click", clickForComputer);
+        grids[i].textContent = board[i];
+      } else {
+        grids[i].addEventListener("click", clickHandler);
+        grids[i].textContent = board[i];
+      }
     }
   };
   // initial render
